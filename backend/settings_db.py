7b48c5f4e48ed05f7b2659e27ab63a4d51dc64f0
serg-shkviro <44038431+serg-shkviro@users.noreply.github.com>
2023-12-15 16:50:43 +0300

    Update settings_db.py

--- backend/settings_db.py
+++ backend/settings_db.py
@@ -1,20 +1,12 @@
 from pydantic_settings import BaseSettings
 import os
 
-username = 'postgres'
-password = 'adminpass'
-host = "127.0.0.1"
-port = 5432
-database = 'model_result'
-
-
-
 class Settings(BaseSettings):
-    username: str = os.environ.get("username_db", "")
+    admin: str = os.environ.get("username_db", "")
     password: str = os.environ.get("password", "")
-    DATABASE_URL: str = os.environ.get("host", "")
+    host: str = os.environ.get("host", "")
     port : int = os.environ.get("port", 0)
     database : str = os.environ.get("database", "")
     
 
-settings_a = Settings()
+settings_DB = Settings()
