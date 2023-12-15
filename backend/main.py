034f8584db1e45622b61ce3201b8a31fdd50709b
serg-shkviro <44038431+serg-shkviro@users.noreply.github.com>
2023-12-15 03:17:26 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,57 +1,65 @@
-from fastapi import FastAPI, Query, Header, Depends
-from fastapi.security import HTTPBearer
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBasicCredentials, HTTPBasic
 import pydantic
 import psycopg2
-from sqlalchemy import create_engine, Column, Integer, Text, JSON, Boolean
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.ext.declarative import declarative_base
 from settings_db import *
-
+from typing import Annotated
+import json
 
 app = FastAPI()
-security = HTTPBearer()
-Base = declarative_base()
+security = HTTPBasic()
 
+'''
+не настроен settings!!!!
+'''
 DATABASE_URL = f"postgresql://{username}:{password}@{host}:{port}/{database}"
 
 class User(pydantic.BaseModel):
-    base_auth: str
     user_id: int
     user_data: str
     product_data: str
     channel_data: str
 
-class InfoText(Base):
-    __tablename__ = 'info_text'
 
-    id = Column(Integer, primary_key=True, index=True)
-    json_input = Column(JSON)
-    text = Column(Text)
-    result = Column(Boolean)
+class UserCredentials(pydantic.BaseModel):
+    username: str
+    password: str
 
-
-def get_info_texts(db: Session):
-    return db.query(InfoText).all()
 
 def all_record():
-    engine = create_engine(DATABASE_URL)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    db = SessionLocal()
-    info_texts = get_info_texts(db)
-    db.close()
-    d = {}
-    for info_text in info_texts:
-        print(f"ID: {info_text.id}, JSON Input: {info_text.json_input}, Text: {info_text.text}, Result: {info_text.result}")
-        d[info_text.id]  = info_text.text
+    con = psycopg2.connect(
+        database=database,
+        user=username,
+        password=password,
+        host=host,
+        port=port
+    )
 
-    return d
-   
+    cur = con.cursor()
+    info_texts = get_info_texts_psycopg2(cur)
+    cur.close()
+    con.close()
+
+    response_json = {}
+    for info_text in info_texts:
+        print(f"ID: {info_text[0]}, JSON Input: {info_text[1]}, Text: {info_text[2]}, Result: {info_text[3]}")
+        response_json[info_text[0]] = info_text[2]
+
+    return response_json
+
+def get_info_texts_psycopg2(cursor):
+    cursor.execute("SELECT * FROM info_text")
+    info_texts = cursor.fetchall()
+    return info_texts
+
+
 @app.get("/texts")
-def get_all_text():
+async def get_all_text():
     return all_record()
 
+
 @app.put("/texts/{id}")
-def change_result(id):
+async def change_result(id):
     con = psycopg2.connect(
         database=database,
         user=username,
@@ -67,7 +75,9 @@ def change_result(id):
 
 
 @app.post("/api/v1/data")
-async def read_item(user: User = Depends()):    
+async def read_item(credentials : Annotated[HTTPBasicCredentials, 
+                                            Depends(security)], user: User = Depends(), 
+):    
     user_data_dict = {
         "user_id": user.user_id,
         "user_data": user.user_data,
@@ -75,21 +85,43 @@ async def read_item(user: User = Depends()):
         "channel_data": user.channel_data,
     }
     
-    engine = create_engine(DATABASE_URL)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    db = SessionLocal()
+    con = psycopg2.connect(
+    database=database,
+    user=username,
+    password=password,
+    host=host,
+    port=port
+    )
 
-    #TODO
-    text = "Example text"
-    result = False
-    #TODO
+    cur = con.cursor()
+    cur.execute("SELECT * FROM admin WHERE username = %s AND password = %s", (credentials.username, credentials.password))
+    admin_user = cur.fetchone()
 
-    new_data = InfoText(json_input=user_data_dict, text="Example text", result=result)
-    db.add(new_data)
-    db.commit()
-    db.close()
-    
-    all_record()
+    if admin_user:
+        user_data_dict = {
+            "user_id": user.user_id,
+            "user_data": user.user_data,
+            "product_data": user.product_data,
+            "channel_data": user.channel_data,
+        }
 
+        # TODO: 
+        text = "Example text"
+        # TODO:
+        cur.execute("INSERT INTO info_text (json_input, text, result) VALUES (%s, %s, %s)",
+        (json.dumps(user_data_dict), text, False),
+    )
+
+        con.commit()
+        cur.close()
+        con.close()
+    else:
+        cur.close()
+        con.close()
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
 '''uvicorn main:app --host 127.0.0.1 --port 8000 --reload'''
