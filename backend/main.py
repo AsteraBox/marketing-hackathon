96dbf69253e339b08b4d3fa4505c71aba3a466cf
Mark Polyak <markpolyak@gmail.com>
2023-12-17 21:35:16 +0200

    Update input for model interaction endpoint

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,50 @@ from settings_db import settings_DB
 from typing import Annotated
 from fastapi import FastAPI, Depends, HTTPException, status, Path
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
+from fastapi import HTTPException
 
 
 app = FastAPI()
 
 security = HTTPBasic()
+
+
+# список всех продуктов
+PRODUCTS = {
+    "ПК": "Классический потребительский кредит",
+    "TOPUP": "Рефинансирование внутреннего ПК в Газпромбанке",
+    "REFIN": "Рефинансирование внешнего ПК в другом банке",
+    "CC": "Кредитная карта",
+    "AUTO": "Классический автокредит",
+    "AUTO_SCR": "Кредит под залог авто",
+    "MORTG": "Ипотека (обычная, льготная, ИТ, дальневосточная и тд)",
+    "MORTG_REFIN": "Рефинансирование ипотеки",
+    "MORTG_SCR": "Кредит под залог недвижимости",
+    "DEPOSIT": "Депозит",
+    "SAVE_ACC": "Накопительный счет",
+    "DC": "Дебетовая карта (МИР, UNION PAY, и тд)",
+    "PREMIUM": "Премиальная карта",
+    "INVEST": "Брокерский и инвестиционный счет (акции, облигации, ПИФ, валюта)",
+    "ISG": "Инвестиционное страхование жизни",
+    "NSG": "Накопительное страхование жизни",
+    "INS_LIFE": "Страхование жизни",
+    "INS_PROPERTY": "Страхование жизни",
+    "TRUST": "Доверительное управление",
+    "OMS": "Обезличенный металлический счет",
+    "IZP": "Индивидуальный зарплатный проект",
+    "CURR_EXC": "Обмен валюты",
+}
+
+# список всех каналов
+CHANNELS = {
+    "SMS": "СМС",
+    "PUSH": "Пуш в мобильном банке",
+    "EMAIL": "Емэйл",
+    "MOB_BANNER": "Текст для баннера в мобильном приложении",
+    "OFFICE_BANNER": "Текст для баннера для менеджера в доп офисе",
+    "MOBILE_CHAT": "Предложение в чате мобильном банке",
+    "KND": "Продажный текст для курьера на дом",
+}
 
 
 def connect_db(settings_DB):
@@ -23,11 +62,34 @@ def connect_db(settings_DB):
     return con
 
 
-class User(pydantic.BaseModel):
-    user_id: int
-    user_data: str
-    product_data: str
-    channel_data: str
+class Client(pydantic.BaseModel):
+    gender: int = None
+    age: float = None
+    reg_region_nm: str = None
+    cnt_tr_all_3m: int = None
+    cnt_tr_top_up_3m: int = None
+    cnt_tr_cash_3m: int = None
+    cnt_tr_buy_3m: int = None
+    cnt_tr_mobile_3m: int = None
+    cnt_tr_oil_3m: int = None
+    cnt_tr_on_card_3m: int = None
+    cnt_tr_service_3m: int = None
+    cnt_zp_12m: int = None
+    sum_zp_12m: float = None
+    limit_exchange_count: int = None
+    max_outstanding_amount_6m: float = None
+    avg_outstanding_amount_3m: float = None
+    cnt_dep_act: int = None
+    sum_dep_now: float = None
+    avg_dep_avg_balance_1month: float = None
+    max_dep_avg_balance_3month: float = None
+    app_vehicle_ind: int = None
+    app_position_type_nm: str = None
+    visit_purposes: str = None
+    qnt_months_from_last_visit: int = None
+    super_clust: str
+    product: str
+    channel: str
 
 
 class UserCredentials(pydantic.BaseModel):
@@ -138,33 +200,26 @@ async def change_result(
 
 @app.post("/api/v1/data")
 async def read_item(credentials : Annotated[HTTPBasicCredentials, 
-                                            Depends(security)], user: User = Depends(), 
+                                            Depends(security)], client: Client, 
 ):    
-    user_data_dict = {
-        "user_id": user.user_id,
-        "user_data": user.user_data,
-        "product_data": user.product_data,
-        "channel_data": user.channel_data,
-    }
-    
     con = connect_db(settings_DB)
     cur = con.cursor()
     cur.execute("SELECT * FROM admin WHERE username = %s AND password = %s", (credentials.username, credentials.password))
     admin_user = cur.fetchone()
 
     if admin_user:
-        user_data_dict = {
-            "user_id": user.user_id,
-            "user_data": user.user_data,
-            "product_data": user.product_data,
-            "channel_data": user.channel_data,
-        }
-
+        print(client)
+        # валидация
+        if client.product not in PRODUCTS:
+            raise HTTPException(status_code=400, detail="Unknown product name")
+        if client.channel not in CHANNELS:
+            raise HTTPException(status_code=400, detail="Unknown channel name")
+        # используем поля в client (напр., client.gender) для доступа к информации о клиенте, продукте и канале
         # TODO: 
         text = "Example text"
         # TODO:
         cur.execute("INSERT INTO info_text (json_input, text, result) VALUES (%s, %s, %s)",
-        (json.dumps(user_data_dict), text, False),
+        (json.dumps(dict(client)), text, False),
     )
 
         con.commit()
