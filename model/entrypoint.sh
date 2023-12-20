a389c449e7bd6d77ce029ff008ae9f9549354cf3
Denis Golod <75615922+Gufderald@users.noreply.github.com>
2023-12-20 23:49:02 +0300

    Update entrypoint.sh

--- model/entrypoint.sh
+++ model/entrypoint.sh
@@ -3,7 +3,7 @@ if [ -f /app/weights/model-q4_K.gguf ]; then
   echo "Weigths file exists, starting app..."
 else
   echo "Weigts file does not exist, downloading..."
-  wget https://huggingface.co/IlyaGusev/saiga_mistral_7b_gguf/resolve/main/model-q4_K.gguf
+  wget -P /app/weights/ https://huggingface.co/IlyaGusev/saiga_mistral_7b_gguf/resolve/main/model-q4_K.gguf
   echo "Download complete, starting app..."
 fi
 uvicorn app:app --host 0.0.0.0 --port 8000 --reload
